eb41ec7670a89bc4c1ae01a185f9682badf52be1
Johnny Lau <83278561+FeeshSushi@users.noreply.github.com>
2026-02-22 23:19:20 -0800

    Initial Commit

---
+++ VEDA/VEDAApp.swift
@@ -0,0 +1,32 @@
+//
+//  VEDAApp.swift
+//  VEDA
+//
+//  Created by Johnny Lau on 2026-02-22.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VEDAApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
